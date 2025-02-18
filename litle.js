6c948293e4dd05f8390f29e484e6f09c2b6077f6
nikonorov_kilill <kirnikmsk@gmail.com>
2025-02-18 20:14:50 +0300

    Разрешение конфликтов

---
+++ litle.js
@@ -0,0 +1,3 @@
+document.getElementById('actionButton').addEventListener('click', function() {
+    this.style.backgroundColor = '#ff0000';
+});
---
+++ server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+    let extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+    }
+
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // Страница не найдена
+                fs.readFile(path.join(__dirname, '404.html'), (err, content) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content, 'utf8');
+                });
+            } else {
+                // Ошибка сервера
+                res.writeHead(500);
+                res.end(`Ошибка сервера: ${err.code}`);
+            }
+        } else {т
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
+});
